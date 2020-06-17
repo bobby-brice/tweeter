418d059dd42b6f6b1f70ba9c21ff61fd21a518e2
bobby-brice <bobby.brice@gmail.com>
2020-06-17 01:13:00 +0000

    minor changes to the successful response object within the AJAX calls

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,29 +7,29 @@
 // Fake data taken from initial-tweets.json
 $(document).ready(function() {
 
-  const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-  ];
+  // const data = [{
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // },
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd"
+  //   },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
+  // ];
 
 
   
@@ -82,10 +82,10 @@ $(document).ready(function() {
     })
       .then((res) => {
         console.log(res);
-        renderTweets(res);
+        // renderTweets(res);
       });
 
-    $('#tweet-text').val(""); //removes the tweet
+    $('#tweet-text').val(""); //removes the tweet field
   });
 
   const loadTweets = () => {
@@ -95,10 +95,10 @@ $(document).ready(function() {
       method: 'GET',
       url,
     }).then((data) => {
-      console.log(data);
+      renderTweets(data);
     }).fail((err) => console.log(err));
   };
 
   loadTweets();
-  renderTweets(data);
+
 });
